dbad5d49a84f2c3bb9cd59dfe723054d5175e19b
Dimitri Sokolyuk <demon@dim13.org>
2014-04-04 18:22:52 +0200

    indent

--- mix-mix.c
+++ mix-mix.c
@@ -1,22 +1,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *strcutm(char *str, const char *cut)
+char *
+strcutm(char *str, const char *cut)
 {
-    char *token, *rest = str;
-    size_t slen = strlen(str);
-    size_t clen = strlen(cut);
+	char *token, *rest = str;
+	size_t slen = strlen(str);
+	size_t clen = strlen(cut);
 
-    if (!clen) return str;
-    while ((token = strsep(&rest, " \t"))) {
-        size_t tlen = rest - token - 1;
-        if (!strcmp(token, cut)) {
-            slen -= clen + 1;
-            tlen  = slen - (token - str) + 1;
-            rest  = memmove(token, rest, tlen);
-        } else if (!rest) continue;
-        token[tlen] = token[tlen] ? '\0' : ' ';
-    }
+	if (!clen)
+		return str;
+	while ((token = strsep(&rest, " \t"))) {
+		size_t tlen = rest - token - 1;
+		if (!strcmp(token, cut)) {
+			slen -= clen + 1;
+			tlen = slen - (token - str) + 1;
+			rest = memmove(token, rest, tlen);
+		} else if (!rest)
+			continue;
+		token[tlen] = token[tlen] ? '\0' : ' ';
+	}
 
-    return str;
+	return str;
 }
--- puzan.c
+++ puzan.c
@@ -1,44 +1,44 @@
-char*
-str_drop_str(char* str, char* sub)
+char *
+str_drop_str(char *str, char *sub)
 {
-    char* p_dst = str;
-    char* p_src = str;
-    char src, subc;
+	char *p_dst = str;
+	char *p_src = str;
+	char src, subc;
 
-    char* p_sub = sub;
-    int sub_len = 0;
+	char *p_sub = sub;
+	int sub_len = 0;
 
-    int delim = 1;
+	int delim = 1;
 
-    do {
-        src = *p_src;
-        subc = *p_sub;
-        
-        if (!subc && (src == ' ')) {
-            p_dst -= sub_len;
-            p_sub = sub;
-            sub_len = 0;
-        } else
-            if (delim && (src == subc)) {
-                p_sub ++;
-                sub_len ++;
-            } else {
-                delim = (src == ' ') ? 1 : 0;
-                p_sub = sub;
-                sub_len = 0;
-            }
+	do {
+		src = *p_src;
+		subc = *p_sub;
 
-        if (p_dst != p_src)
-            *p_dst = src;
+		if (!subc && (src == ' ')) {
+			p_dst -= sub_len;
+			p_sub = sub;
+			sub_len = 0;
+		} else if (delim && (src == subc)) {
+			p_sub++;
+			sub_len++;
+		} else {
+			delim = (src == ' ') ? 1 : 0;
+			p_sub = sub;
+			sub_len = 0;
+		}
 
-        p_src ++;
-        p_dst ++;
+		if (p_dst != p_src)
+			*p_dst = src;
 
-    } while(src);
+		p_src++;
+		p_dst++;
 
-    if (!subc) p_dst -= sub_len;
-    
-    *p_dst = 0;
+	} while (src);
 
-    return str;
+	if (!subc)
+		p_dst -= sub_len;
+
+	*p_dst = 0;
+
+	return str;
 }
