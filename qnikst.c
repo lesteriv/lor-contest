ac359b0f1324ca84d210d98c504eea01e1083466
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-04-04 17:06:24 +0400

    add qnikst`s solution
    + fix access violation error found by anonymous
    + speedup fix (remote length check)
    + Speedup calculate length of the in strict only if required and cache
      result

--- qnikst.c
+++ qnikst.c
@@ -2,28 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *
-undebugq(char *in, char *needle)
-{
-	char *p = in;
-	size_t l = 0, nl = strlen(needle);
+char * undebugq(char *in, char *needle) {
+    char *p = in;
+    size_t l = 0, nl = strlen(needle);
 
-	for (; *p;) {
-		for (; *p == ' '; p++);
-		if (*p == *needle) {
-			if (!strncmp(p, needle, nl)) {
-				if (p[nl] == ' ' || p[nl] == '\t') {
-					if (!l)
-						l = strlen(p);
-					memmove(p, p + nl + 1, l - nl + 1);	// we need to copy \0
-					continue;
-				} else if (p[nl] == '\0') {
-					*p = '\0';
-					return in;
-				}
-			}
-		}
-		for (; (*p != ' ' || *p != '\t') && *p; p++);
-	}
-	return in;
+    for (; *p;) {
+        for (; *p == ' '; p++);
+        if (*p == *needle) {
+            if (! strncmp(p, needle, nl)) {
+                if (!l) l = strlen(p);
+                if (p[nl] == ' ') {
+                  memmove(p, p + nl + 1, l - nl + 1); // we need to copy \0
+                  l -= nl;
+                  continue;
+                } else if (p[nl] == '\0') {
+                  *p = '\0';
+                  return in;
+                } else if (l < nl) {
+                  return in;
+                } 
+                p += nl;
+            }
+        }
+        for (; *p != ' ' && *p; p++);
+        //for (; ! (*p == ' ' || *p == '\t') && *p; p++);
+    }
+    return in;
 }
