6c17c1d7807cdc26eb1041ffe61a478e3976b8ac
Dimitri Sokolyuk <demon@dim13.org>
2014-04-05 09:44:05 +0200

    indent

--- wota.c
+++ wota.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static void 
+static void
 strremove(char *s, const char *t)
 {
 	size_t l = strlen(t);
@@ -28,7 +28,7 @@ strremove_wrapper(char *hay, char *needle)
 	return hay;
 }
 
-static void 
+static void
 remove_word(char *s, const char *w)
 {
 	int l = strlen(w);
@@ -50,41 +50,31 @@ remove_word_wrapper(char *hay, char *needle)
 	return hay;
 }
 
-static void 
+static void
 undebug_wota(char *s)
 {
-	long v = *((long*)s);
-	if( ( v & 0x000000FFFFFFFFFF ) == 0x6775626564 )
-	{
-		char s5 = ( v & 0x0000FF0000000000 ) >> 40;
-		if( s5 )
-		{
-			if( s5 == 32 )
-				memset( s, ' ', 5 );
-				
+	long v = *((long *)s);
+	if ((v & 0x000000FFFFFFFFFF) == 0x6775626564) {
+		char s5 = (v & 0x0000FF0000000000) >> 40;
+		if (s5) {
+			if (s5 == 32)
+				memset(s, ' ', 5);
+
 			s += 6;
-		}
-		else
-		{
+		} else {
 			*s = 0;
 			return;
 		}
 	}
-
-	while(( s = strchr( ++s, ' ' ) ))
-	{
-		v = *((long*)s);
-		if( ( v & 0x0000FFFFFFFFFFFF ) == 0x677562656420 )
-		{
-			char s6 = ( v & 0x00FF000000000000 ) >> 48;
-			if( !s6 )
-			{
+	while ((s = strchr(++s, ' '))) {
+		v = *((long *)s);
+		if ((v & 0x0000FFFFFFFFFFFF) == 0x677562656420) {
+			char s6 = (v & 0x00FF000000000000) >> 48;
+			if (!s6) {
 				*s = 0;
 				break;
-			}
-			else if( s6 == ' ' )
-			{
-				memset( s + 1, ' ', 5 );
+			} else if (s6 == ' ') {
+				memset(s + 1, ' ', 5);
 				s += 6;
 			}
 		}
