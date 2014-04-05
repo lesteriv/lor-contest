4c0eca6121a8a461da5bcf5ff54f19bd868c79c4
Igor Nikitin <igor_nikitin@valentina-db.com>
2014-04-05 06:07:08 +0300

    remove strremove solution from wota

--- wota.c
+++ wota.c
@@ -8,93 +8,73 @@
 #include <stdio.h>
 #include <string.h>
 
-void 
-strremove(char *s, const char *t)
-{
-	size_t l = strlen(t);
-	char *d = s;
-
-	do
-		while (!strncmp(s, t, l))
-			s += l;
-	while ((*d++ = *s++));
-}
-
-char *
-strremove_wrapper(char *hay, char *needle)
-{
-	strremove(hay, needle);
-
-	return hay;
-}
-
-void 
+void
 remove_word(char *s, const char *w)
 {
-	int l = strlen(w);
-	char *p = s;
+    int l = strlen(w);
+    char *p = s;
 
-	while ((p = strstr(p, w))) {
-		if ((p == s || isspace(p[-1])) && (!p[l] || isspace(p[l])))
-			memset(p, ' ', l);
+    while ((p = strstr(p, w))) {
+        if ((p == s || isspace(p[-1])) && (!p[l] || isspace(p[l])))
+            memset(p, ' ', l);
 
-		p += l;
-	}
+        p += l;
+    }
 }
 
 char *
 remove_word_wrapper(char *hay, char *needle)
 {
-	remove_word(hay, needle);
+    remove_word(hay, needle);
 
-	return hay;
+    return hay;
 }
 
-void 
+void
 undebug_wota(char *s)
 {
-	long v = *((long*)s);
-	if( ( v & 0x000000FFFFFFFFFF ) == 0x6775626564 )
-	{
-		char s5 = ( v & 0x0000FF0000000000 ) >> 40;
-		if( s5 )
-		{
-			if( s5 == 32 )
-				memset( s, ' ', 5 );
-				
-			s += 6;
-		}
-		else
-		{
-			*s = 0;
-			return;
-		}
-	}
+    long v = *((long*)s);
+    if( ( v & 0x000000FFFFFFFFFF ) == 0x6775626564 )
+    {
+        char s5 = ( v & 0x0000FF0000000000 ) >> 40;
+        if( s5 )
+        {
+            if( s5 == 32 )
+                memset( s, ' ', 5 );
 
-	while(( s = strchr( ++s, ' ' ) ))
-	{
-		v = *((long*)s);
-		if( ( v & 0x0000FFFFFFFFFFFF ) == 0x677562656420 )
-		{
-			char s6 = ( v & 0x00FF000000000000 ) >> 48;
-			if( !s6 )
-			{
-				*s = 0;
-				break;
-			}
-			else if( s6 == ' ' )
-			{
-				memset( s + 1, ' ', 5 );
-				s += 6;
-			}
-		}
-	}
+            s += 6;
+        }
+        else
+        {
+            *s = 0;
+            return;
+        }
+    }
+
+    while(( s = strchr( ++s, ' ' ) ))
+    {
+        v = *((long*)s);
+        if( ( v & 0x0000FFFFFFFFFFFF ) == 0x677562656420 )
+        {
+            char s6 = ( v & 0x00FF000000000000 ) >> 48;
+            if( !s6 )
+            {
+                *s = 0;
+                break;
+            }
+            else if( s6 == ' ' )
+            {
+                memset( s + 1, ' ', 5 );
+                s += 6;
+            }
+        }
+    }
 }
 
 char *
 undebug_wota_wrapper(char *hay, char *needle)
 {
-	undebug_wota(hay);
+    undebug_wota(hay);
 
-	return hay;
+    return hay;
 }
