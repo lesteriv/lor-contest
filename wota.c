df4cb6a5fb9df44ed9f07a4faeb3a435a2d62b59
lesteriv <zxfox@mail.ru>
2014-04-06 07:43:38 +0300

    faster remove_word from wota

--- wota.c
+++ wota.c
@@ -11,48 +11,27 @@
 static void
 strremove(char *s, const char *t)
 {
-	size_t l = strlen(t);
-	char *d = s;
+    size_t l = strlen(t);
+    char *d = s;
 
-	do
-		while (!strncmp(s, t, l))
-			s += l;
-	while ((*d++ = *s++));
+    do
+        while (!strncmp(s, t, l))
+            s += l;
+    while ((*d++ = *s++));
 }
 
 char *
 strremove_wrapper(char *hay, char *needle)
 {
-	strremove(hay, needle);
+    strremove(hay, needle);
 
-	return hay;
-}
-
-static void
-remove_word(char *s, const char *w)
-{
-	int l = strlen(w);
-	char *p = s;
-
-	while ((p = strstr(p, w))) {
-		if ((p == s || isspace(p[-1])) && (!p[l] || isspace(p[l])))
-			memset(p, ' ', l);
-
-		p += l;
-	}
-}
-
-char *
-remove_word_wrapper(char *hay, char *needle)
-{
-	remove_word(hay, needle);
-
-	return hay;
+    return hay;
 }
 
 #if __linux__
 extern char *strchrnul(const char *__s, int __c);
 #endif
+
 
 static void
 undebug_wota(char *s)
@@ -65,39 +44,72 @@ undebug_wota(char *s)
             memset(s, ' ', 5);
     }
 #else
-	long v = *((long *)s);
-	if ((v & 0x000000FFFFFFFFFF) == 0x6775626564) {
-		char s5 = (v & 0x0000FF0000000000) >> 40;
-		if (s5) {
-			if (s5 == 32)
-				memset(s, ' ', 5);
+    long v = *((long *)s);
+    if ((v & 0x000000FFFFFFFFFF) == 0x6775626564) {
+        char s5 = (v & 0x0000FF0000000000) >> 40;
+        if (s5) {
+            if (s5 == 32)
+                memset(s, ' ', 5);
 
-			s += 6;
-		} else {
-			*s = 0;
-			return;
-		}
-	}
-	while ((s = strchr(++s, ' '))) {
-		v = *((long *)s);
-		if ((v & 0x0000FFFFFFFFFFFF) == 0x677562656420) {
-			char s6 = (v & 0x00FF000000000000) >> 48;
-			if (!s6) {
-				*s = 0;
-				break;
-			} else if (s6 == ' ') {
-				memset(s + 1, ' ', 5);
-				s += 6;
-			}
-		}
-	}
+            s += 6;
+        } else {
+            *s = 0;
+            return;
+        }
+    }
+    while ((s = strchr(++s, ' '))) {
+        v = *((long *)s);
+        if ((v & 0x0000FFFFFFFFFFFF) == 0x677562656420) {
+            char s6 = (v & 0x00FF000000000000) >> 48;
+            if (!s6) {
+                *s = 0;
+                break;
+            } else if (s6 == ' ') {
+                memset(s + 1, ' ', 5);
+                s += 6;
+            }
+        }
+    }
 #endif
 }
 
 char *
 undebug_wota_wrapper(char *hay, char *needle)
 {
-	undebug_wota(hay);
+    undebug_wota(hay);
 
-	return hay;
+    return hay;
+}
+
+static void
+remove_word(char *s, const char *w)
+{
+    if( w == "debug" ) // optimizatio for most used case
+        return undebug_wota( s );
+
+    int l = strlen(w);
+    char *p = s;
+
+#if __linux__
+    for (; *p; s = p + 1) {
+        p = strchrnul(s, ' ');
+        if (p - s == l && !memcmp( s, w, l ))
+            memset(s, ' ', l);
+    }
+#else
+    while ((p = strstr(p, w))) {
+        if ((p == s || isspace(p[-1])) && (!p[l] || isspace(p[l])))
+            memset(p, ' ', l);
+
+        p += l;
+    }
+#endif
+}
+
+char *
+remove_word_wrapper(char *hay, char *needle)
+{
+    remove_word(hay, needle);
+
+    return hay;
 }
